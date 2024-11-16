ed068a38f870c78cf21d1735047db1a80793028b
hanwj75 <wjws9608@gmail.com>
2024-11-17 04:29:28 +0900

    Update : "DB연결 테스트"
    DB 연결 테스트 완료

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,27 +1,28 @@
 import envFiles from '../constants/env.js';
 
+const env = envFiles.database;
 export const dbConfig = {
   database: {
     USER_DB: {
-      host: envFiles.DB1_HOST,
-      name: envFiles.DB1_NAME,
-      password: envFiles.DB1_PASSWORD,
-      port: envFiles.DB1_PORT,
-      user: envFiles.DB1_USER,
+      host: env.DB1_HOST,
+      port: env.DB1_PORT,
+      user: env.DB1_USER,
+      password: env.DB1_PASSWORD,
+      database: env.DB1_NAME,
     },
     RECORD_DB: {
-      host: envFiles.DB2_HOST,
-      name: envFiles.DB2_NAME,
-      password: envFiles.DB2_PASSWORD,
-      port: envFiles.DB2_PORT,
-      user: envFiles.DB2_USER,
+      host: env.DB2_HOST,
+      port: env.DB2_PORT,
+      user: env.DB2_USER,
+      password: env.DB2_PASSWORD,
+      database: env.DB2_NAME,
     },
     CARD_DB: {
-      host: envFiles.DB3_HOST,
-      name: envFiles.DB3_NAME,
-      password: envFiles.DB3_PASSWORD,
-      port: envFiles.DB3_PORT,
-      user: envFiles.DB3_USER,
+      host: env.DB3_HOST,
+      port: env.DB3_PORT,
+      user: env.DB3_USER,
+      password: env.DB3_PASSWORD,
+      database: env.DB3_NAME,
     },
   },
 };
---
+++ src/db/database.js
@@ -0,0 +1,41 @@
+import mysql from 'mysql2/promise';
+import { dbConfig } from '../config/dbconfig.js';
+import { formatDate } from '../utils/dateFomatter.js';
+
+const database = dbConfig.database;
+
+// 데이터베이스 커넥션 풀 생성 함수
+const createPool = (dbConfig) => {
+  const pool = mysql.createPool({
+    host: dbConfig.host,
+    port: dbConfig.port,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.name,
+    waitForConnections: true,
+    connectionLimit: 10, // 커넥션 풀에서 최대 연결 수
+    queueLimit: 0, // 0일 경우 무제한 대기열
+  });
+
+  const originalQuery = pool.query;
+
+  pool.query = (sql, params) => {
+    const date = new Date();
+    // 쿼리 실행시 로그
+    console.log(
+      `[${formatDate(date)}] Executing query: ${sql} ${params ? `, ${JSON.stringify(params)}` : ``}`,
+    );
+    return originalQuery.call(pool, sql, params);
+  };
+
+  return pool;
+};
+
+// 여러 데이터베이스 커넥션 풀 생성
+const pools = {
+  USER_DB: createPool(database.USER_DB),
+  RECORD_DB: createPool(database.RECORD_DB),
+  CARD_DB: createPool(database.CARD_DB),
+};
+
+export default pools;
