62d128ca14dba70a06b7cf048d1042e58836a9df
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-11-19 00:21:58 +0900

    Fix : 회원가입 Response 구조 수정

--- src/handler/user/registerHandler.js
+++ src/handler/user/registerHandler.js
@@ -19,9 +19,11 @@ const registerHandler = async (socket, payload) => {
 
     if (checkExistId) {
       const responsePayload = {
-        success: false,
-        message: '이미 존재하는 ID',
-        failCode: 2,
+        registerResponse: {
+          success: false,
+          message: '이미 존재하는 ID',
+          failCode: 2,
+        },
       };
       const registerResponse = createResponse(responsePayload, packetType.REGISTER_RESPONSE, 0);
       socket.write(registerResponse);
@@ -32,9 +34,11 @@ const registerHandler = async (socket, payload) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const createUserId = await createUser(email, nickname, hashedPassword);
     const responsePayload = {
-      success: true,
-      message: 'Register Success',
-      failCode: 0,
+      registerResponse: {
+        success: true,
+        message: 'Register Success',
+        failCode: 0,
+      },
     };
 
     const registerResponse = createResponse(responsePayload, packetType.REGISTER_RESPONSE, 0);
