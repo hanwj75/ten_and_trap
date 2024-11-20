328f975fc3be182ca81cf6519c9203774b68620f
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-11-20 12:22:09 +0900

    Add : 캐릭터 클래스 추가

---
+++ src/classes/models/character.class.js
@@ -0,0 +1,27 @@
+class Character {
+  constructor(
+    characterType,
+    roleType,
+    hp,
+    weapon,
+    stateInfo,
+    equips,
+    debuffs,
+    handCards,
+    bbangCount,
+    handCardsCount,
+  ) {
+    this.characterType = characterType;
+    this.roleType = roleType;
+    this.hp = hp;
+    this.weapon = weapon;
+    this.stateInfo = stateInfo;
+    this.equips = equips;
+    this.debuffs = debuffs;
+    this.handCards = handCards;
+    this.bbangCount = bbangCount;
+    this.handCardsCount = handCardsCount;
+  }
+}
+
+export default Character;
