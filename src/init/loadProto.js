66f83504f2cff97ca7b0af133b4b2167bbc69ebc
hanwj75 <wjws9608@gmail.com>
2024-11-21 19:30:00 +0900

    Update : "loadProto Enum 추가구성"

--- src/init/loadProto.js
+++ src/init/loadProto.js
@@ -8,7 +8,11 @@ const PROTO_PATH = path.resolve(__dirname, '../protobuf/packets.proto'); // .pro
 
 export let GamePacket = null;
 export let GlobalFailCode = null;
-
+export let CardType = null;
+export let RoleType = null;
+export let RoomStateType = null;
+export let CharacterStateType = null;
+export let CharacterType = null;
 export const loadProtos = async () => {
   try {
     const root = await protobuf.load(PROTO_PATH);
@@ -18,9 +22,28 @@ export const loadProtos = async () => {
     }
 
     GlobalFailCode = root.lookupEnum('GlobalFailCode');
-
     if (GlobalFailCode) {
       console.log(`성공적으로 로드됨: ${GlobalFailCode}`);
+    }
+    CardType = root.lookupEnum('CardType');
+    if (CardType) {
+      console.log(`성공적으로 로드됨: ${CardType}`);
+    }
+    RoleType = root.lookupEnum('RoleType');
+    if (RoleType) {
+      console.log(`성공적으로 로드됨: ${RoleType}`);
+    }
+    RoomStateType = root.lookupEnum('RoomStateType');
+    if (RoomStateType) {
+      console.log(`성공적으로 로드됨: ${RoomStateType}`);
+    }
+    CharacterStateType = root.lookupEnum('CharacterStateType');
+    if (CharacterStateType) {
+      console.log(`성공적으로 로드됨: ${CharacterStateType}`);
+    }
+    CharacterType = root.lookupEnum('CharacterType');
+    if (CharacterType) {
+      console.log(`성공적으로 로드됨: ${CharacterType}`);
     }
   } catch (err) {
     console.error('Proto 파일 로드 중 오류 발생:', err);
