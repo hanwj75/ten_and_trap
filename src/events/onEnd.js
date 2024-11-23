82f4bc088a5373c1998d0fe53bf21a70d3fb4fd5
hanwj75 <wjws9608@gmail.com>
2024-11-23 21:36:46 +0900

    Update : "클라 종료시 방 삭제"
    일반 유저가 클라 종료시 나머지 유저에게 노티 쏴줌,
    방장이 클라 종료시 방 삭제후 나머지 로비로 보내줌

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,16 +1,70 @@
 import { getUserBySocket, removeUser } from '../sessions/user.session.js';
 import { redis } from '../init/redis/redis.js';
+import { sendNotificationToUsers } from '../utils/notifications/notification.js';
+import { packetType } from '../constants/header.js';
+import { GlobalFailCode } from '../init/loadProto.js';
 
 export const onEnd = (socket) => async () => {
   console.log('클라이언트 연결이 종료되었습니다.');
 
+  /**
+   * @todo
+   * 1. 유저의 joinRoom을 찾아옴
+   * 2. joinRoom과 일치하는 키값을 가진 room을 찾아옴
+   * 3. 일반유저일경우 해당 유저데이터만 삭제 => 제외 notification 보내줌
+   * 4. 방장일 경우 room키값 삭제 => leaveRoomResponse로 다 쫒아냄
+   */
   const user = await getUserBySocket(socket);
   if (user) {
-    // 방에 들어가 있다면 방에서 나가게
-    // 만약 호스트라면 그 다음 사람에게 호스트를 넘겨주게
-    // 일단 생각으로는 shift를 통해 맨 앞에 사람을 빼주는 식으로 할까 고민중
-    // 앞에서부터 호스트와 먼저 들어온 사람 순
     await redis.delRedisByKey(`user:${user.userId}`);
     await removeUser(socket);
+  } else if (!user) {
+    console.error(`존재하지 않는 유저입니다.`);
+  }
+
+  const currentUserId = user.id;
+  //현재 나가려하는 방의 키값
+  const leaveRequestRoomId = await redis.getRoomByUserId(`user:${currentUserId}`, `joinRoom`);
+  //나가는 유저의 정보
+  const getLeaveUserId = await redis.getRoomByUserId(`room:${leaveRequestRoomId}`, `users`);
+  //해당 방의 방장
+  const getOwnerId = await redis.getRoomByUserId(`room:${leaveRequestRoomId}`, `ownerId`);
+
+  const users = JSON.parse(getLeaveUserId);
+
+  // 현재 유저의 socket.id에 해당하는 객체의 인덱스를 찾음
+  const userIndex = users.findIndex((user) => user.id === currentUserId);
+  if (userIndex !== -1) {
+    const removeUser = users.splice(userIndex, 1)[0];
+    const roomOwnerId = removeUser.id === Number(getOwnerId);
+
+    const leaveRoomNotificationPayload = {
+      leaveRoomNotification: {
+        userId: removeUser.id,
+      },
+    };
+    sendNotificationToUsers(
+      users,
+      leaveRoomNotificationPayload,
+      packetType.LEAVE_ROOM_NOTIFICATION,
+      0,
+    );
+    await redis.updateUsersToRoom(leaveRequestRoomId, 'users', users);
+    if (roomOwnerId) {
+      const leaveRoomResponsePayload = {
+        leaveRoomResponse: {
+          success: true,
+          failCode: GlobalFailCode.values.NONE_FAILCODE,
+        },
+      };
+      sendNotificationToUsers(users, leaveRoomResponsePayload, packetType.LEAVE_ROOM_RESPONSE, 0);
+
+      // 모든 사용자 상태 업데이트
+      users.forEach(async (user) => {
+        await redis.setRoomByUserId(`user:${user.id}`, `joinRoom`, null);
+      });
+      // Redis에서 방 데이터 삭제
+      await redis.delRedisByKey(`room:${leaveRequestRoomId}`);
+    }
   }
 };
