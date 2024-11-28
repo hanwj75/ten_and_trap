a95aec4e723876c24187a1505a11d3222a05c727
hanwj75 <wjws9608@gmail.com>
2024-11-28 17:18:46 +0900

    Refactor : "login"

--- src/handler/user/loginHandler.js
+++ src/handler/user/loginHandler.js
@@ -41,26 +41,16 @@ export const loginHandler = async (socket, payload) => {
     if (isExistUser) {
       return makeResponse(socket, false, '이미 접속중인 아이디', '', '', failCode.AUTHENTICATION_FAILED);
     }
-    const stateInfo = { state: 0, nextState: 0, nextStateAt: 0, stateTargetUserId: 0 };
-    // 캐릭터 클래스 생성 (캐릭터 종류, 역할, 체력, 무기, 상태, 장비, 디버프, handCards, 뱅카운터, handCardsCount)
 
-    const loginCharacter = new Character(
-      1,
-      3,
-      5,
-      0,
-      stateInfo,
-      [],
-      [],
-      [
-        { type: 1, count: 1 },
-        { type: 2, count: 1 },
-        { type: 3, count: 1 },
-        { type: 4, count: 1 },
-      ],
-      1,
-      4,
-    );
+    // 캐릭터 클래스 생성 (캐릭터 종류, 역할, 체력, 무기, 상태, 장비, 디버프, handCards, 뱅카운터, handCardsCount)
+    const stateInfo = { state: 0, nextState: 0, nextStateAt: 0, stateTargetUserId: 0 };
+    const handCards = [
+      { type: 1, count: 1 },
+      { type: 2, count: 1 },
+      { type: 3, count: 1 },
+      { type: 4, count: 1 },
+    ];
+    const loginCharacter = new Character(1, 3, 7, 0, stateInfo, [], [], handCards, 1, 4);
 
     // 유저 클래스 생성
     const loginUser = new User(checkExistId.id, socket, email, checkExistId.nickName, null, loginCharacter, { x: 0, y: 0 });
